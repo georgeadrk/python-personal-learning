69d3fa5b01f4b94f7058baa0780fa9ef10fef112
[George Kurniawan] <georgeadrielk@gmail.com>
2025-10-27 16:27:10 +0700

    Project 3

--- project3.py
+++ project3.py
@@ -1,6 +1,11 @@
 q: int = int(input('How many expenses today? '))
 total: int = 0
 i = 1
+
+if q == 0:
+    print('No expenses today!')
+    exit()
+
 while i <= q:
     x: int = int(input(f'Expense {i}: '))
     total += x
@@ -8,4 +13,4 @@ while i <= q:
 avg: float = total / q
 
 print(f'\nTotal spent: {total}')
-print(f'Average spending: {avg}')
+print(f'Average spending: {avg:.2f}')
