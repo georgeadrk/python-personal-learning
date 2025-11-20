7c6b9137a0028994e861cd62991003744b46d501
[George Kurniawan] <georgeadrielk@gmail.com>
2025-11-20 14:25:57 +0700

    Project 9 - Class-Based Password Manager

--- project9.py
+++ project9.py
@@ -4,10 +4,39 @@ class PassManager():
         print("1. Add a new account")
         print("2. View saved accounts")
         print("3. Delete all saved data")
-        print("Quit")
+        print("4. Quit")
 
     def addAccount(self) -> None:
-        web = input("Website: ")
-        usn = input("Username: ")
-        pw = input("Password: ")
+        web: str = input("Website: ")
+        usn: str = input("Username: ")
+        pw: str = input("Password: ")
 
+        with open('project9_passwords.txt', 'a') as f:
+            f.write(f'Website: {web}\nUsername: {usn}\nPassword: {pw}\n\n')
+        print('Account added!')
+
+    def viewAccounts(self) -> None:
+        print('Saved accounts:\n')
+        with open('project9_passwords.txt', 'r') as f:
+            content = f.read()
+            print(content)
+
+    def deleteAll(self) -> None:
+        open('project8_passwords.txt', 'w').close()
+        print('All saved data successfully deleted!')
+
+passManager = PassManager()
+
+passManager.showOptions()
+while True:
+    ans: int = int(input("Choose an option: "))
+
+    if ans == 1:
+        passManager.addAccount()
+    elif ans == 2:
+        passManager.viewAccounts()
+    elif ans == 3:
+        passManager.deleteAll()
+    elif ans == 4:
+        print("Goodbye!")
+        break
